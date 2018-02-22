f2fafbc24d5749f0efd42839bf4e8b91973d0721
kinhang <kinhang@trip.io>
2018-02-23 02:45:16 +0800

    remove koa body

--- middlewares/telegramBot/bot.js
+++ middlewares/telegramBot/bot.js
@@ -7,7 +7,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 // Details: https://github.com/telegraf/telegraf/issues/294
 // bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
 
-bot.hears('hey', ({reply}) => {
+bot.hears('yo', ({reply}) => {
 
   reply('Hey there!')
 
@@ -15,6 +15,5 @@ bot.hears('hey', ({reply}) => {
 
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://bot.trip.io/telegramBot')
 
 module.exports = bot
--- middlewares/telegramBot/index.js
+++ middlewares/telegramBot/index.js
@@ -1,10 +1,11 @@
-const koaBody = require('koa-body')
+// const koaBody = require('koa-body')
 const bot = require('./bot')
 
 module.exports = strapi => {
   return {
     initialize: function(cb) {
-      strapi.app.use(koaBody())
+      // bot.telegram.setWebhook('https://bot.trip.io/telegramBot')
+      // strapi.app.use(koaBody())
       strapi.app.use((ctx, next) =>  ctx.url === '/telegramBot'
         ? bot.handleUpdate(ctx.request.body, ctx.response)
         : next()
