868c4b9aa0d376a7ac8c8d8182ad778d7a2f8f4e
kinhang <kinhang@trip.io>
2018-02-22 23:53:39 +0800

    add telegraf as middleware

---
+++ middlewares/telegramBot/index.js
@@ -0,0 +1,30 @@
+const Telegraf = require('telegraf')
+const koaBody = require('koa-body')
+
+
+const bot = new Telegraf('493666190:AAEhSBXNz_oAE1H_Y8zkPEF2nM6mvDyYA4E')
+// First reply will be served via webhook response,
+// but messages order not guaranteed due to `koa` pipeline design.
+// Details: https://github.com/telegraf/telegraf/issues/294
+bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+bot.on('text', ({ reply }) => reply('Hey there!'))
+
+// Set telegram webhook
+// npm install -g localtunnel && lt --port 3000
+bot.telegram.setWebhook('http://tripiobot-c6a75deb-1.b16db5f4.cont.dockerapp.io:1337/telegramBot')
+
+
+module.exports = strapi => {
+  return {
+    initialize: function(cb) {
+      strapi.app.use(koaBody())
+
+      strapi.app.use((ctx, next) =>  ctx.url === '/telegramBot'
+        ? bot.handleUpdate(ctx.request.body, ctx.response)
+        : next()
+      )
+
+      cb();
+    }
+  };
+};
