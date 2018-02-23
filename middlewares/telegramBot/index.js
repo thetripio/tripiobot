10462995835487f07fa69a1fecf2b6baf5de9bc5
kinhang <kinhang@trip.io>
2018-02-23 20:17:57 +0800

    build bot as plugin

--- middlewares/telegramBot/index.js
+++ middlewares/telegramBot/index.js
@@ -6,7 +6,7 @@ module.exports = strapi => {
   return {
     initialize: function (cb) {
       strapi.app.use((ctx, next) => ctx.method === 'POST' && ctx.url === '/telegramBot'
-        ? bot.handleUpdateRaw(ctx, ctx.response)
+        ? bot.handleUpdate(ctx.request.body, ctx.response)
         : next()
       )
       cb();
---
+++ plugins/telegram-bot/controllers/TelegramBot.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * TelegramBot.js controller
+ *
+ * @description: A set of functions called "actions" of the `telegram-bot` plugin.
+ */
+
+const Telegraf = require('telegraf')
+// const bot = new Telegraf('493666190:AAEhSBXNz_oAE1H_Y8zkPEF2nM6mvDyYA4E')
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const parse = require('co-body');
+
+// First reply will be served via webhook response,
+// but messages order not guaranteed due to `koa` pipeline design.
+// Details: https://github.com/telegraf/telegraf/issues/294
+// bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+
+bot.telegram.setWebhook('https://bot.trpio.io/telegram-bot/webhook')
+
+
+bot.hears('yo', ({reply}) => {
+
+  reply('Hey there!')
+
+})
+
+bot.handleUpdateRaw  = async (ctx,response)=>{
+  let update = await parse.json(ctx.req)
+  bot.handleUpdate (update, response)
+}
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  webhook: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    bot.handleUpdate (ctx.request.body, ctx.response)
+    // console.log(ctx.request.body)
+    // ctx.send(ctx);
+  }
+};
