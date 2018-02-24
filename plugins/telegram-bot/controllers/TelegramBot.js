bbb043f6ec638b3fec18849e7f64a682d6076560
kinhang <kinhang@trip.io>
2018-02-24 14:41:13 +0800

    remove bot from controller

--- plugins/telegram-bot/controllers/TelegramBot.js
+++ plugins/telegram-bot/controllers/TelegramBot.js
@@ -1,9 +1,10 @@
-let bot = require('./bot')
+let bot = require('../bot')
 bot.telegram.setWebhook('https://bot.trip.io/telegram-bot/webhook')
 
 module.exports = {
   webhook: async ctx => {
     bot.handleUpdate(ctx.request.body, ctx.response)
+    console.log(ctx.request.body)
     ctx.send('ok')
   }
 }
