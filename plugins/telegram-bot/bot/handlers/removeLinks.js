713c1f690a3ae77dfbf6b2fd3caed06a0ec02181
kinhang <kinhang@trip.io>
2018-02-27 23:13:20 +0800

    remove 0x add

--- plugins/telegram-bot/bot/handlers/removeLinks.js
+++ plugins/telegram-bot/bot/handlers/removeLinks.js
@@ -7,6 +7,16 @@ const removeLinks = {
       let member =  await ctx.getChatMember(message.from.id)
       let isAdmin = (member.status == "creator") || (member.status == "administrator")
       if(!isAdmin){
+        if(message.text){
+          const regex = /^0x[a-fA-F0-9]{40}$/g;
+          const str = message.text;
+          let m;
+          if(regex.exec(str)!== null){
+            ctx.deleteMessage(message.id)
+          }
+        }
+
+
         if(message.entities){
           let hasUrl = false
           for(let entity of message.entities){
